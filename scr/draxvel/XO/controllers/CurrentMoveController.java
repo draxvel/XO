808969b76c4519b02eba018cdb31207812e3c626
draxvel <ntv3331998@gmail.com>
2017-07-22 22:59:19 +0300

    CurrentMoveController impl

---
+++ scr/draxvel/XO/controllers/CurrentMoveController.java
@@ -0,0 +1,42 @@
+package draxvel.XO.controllers;
+
+import draxvel.XO.model.Field;
+import draxvel.XO.model.Figure;
+import draxvel.XO.model.exception.InvalidPointException;
+
+import java.awt.*;
+
+import static draxvel.XO.model.Figure.O;
+import static draxvel.XO.model.Figure.X;
+
+public class CurrentMoveController {
+
+    public Figure currentMove(final Field field){
+        int countFigure = 0;
+
+        for(int i=0; i<field.getSize(); i++){
+            countFigure += countFiguresInTheRow(field,i);
+        }
+        if(countFigure== field.getSize()*field.getSize()) return null;
+
+        if(countFigure %2 ==0) return Figure.X;
+
+        return Figure.O;
+    }
+
+    private int countFiguresInTheRow(final Field field, final  int row){
+        int countFigure = 0;
+        for (int x =0; x<field.getSize(); x++){
+            try {
+                if(field.getFigure(new Point(x, row))!= null)
+                    countFigure++;
+
+            } catch (InvalidPointException e) {
+                e.printStackTrace();
+            } {
+
+            }
+        }
+        return countFigure;
+    }
+}
