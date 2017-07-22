7ec370265fe0fc043bfa9f777859ecfc80cd13e1
draxvel <ntv3331998@gmail.com>
2017-07-22 14:14:30 +0300

    FieldTest - created

---
+++ test/draxvel/XO/model/FieldTest.java
@@ -0,0 +1,30 @@
+package draxvel.XO.model;
+
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+
+public class FieldTest {
+    @Test
+    public void getSize() throws Exception {
+
+        Field field = new Field();
+        assertEquals(3, field.getSize());
+    }
+
+    @Test
+    public void setFigure() throws Exception {
+        Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint,inputFigure);
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertEquals(inputFigure,actualFigure);
+    }
+
+}
